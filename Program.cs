8e65f7f28e55b71fed8eaf0012c290cf87cb43a7
Brian Wood <uoflcardinalsalltheway@yahoo.com>
2017-04-20 21:08:47 -0400

    Vowel Calculator

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            Console.WriteLine("Please enter some text: ");
+            myString = Console.ReadLine();
+
+            myString.ToCharArray();
+            char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+
+            int sum = 0;
+
+            for (int i = 0; i < myString.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (myString[i] == vowels[j])
+                    {
+                        sum++;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
